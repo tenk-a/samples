5d1cd9fae74af4eb4b5241769e6476bae60616e6
tenk-a <tenka@6809.net>
2025-01-06 01:16:44 +0900

    pdcurses_hello: 動作を、カーソル操作する例に変更. win utf-8用 manifest 追加.

--- pdcurses_hello/src/hello.c
+++ pdcurses_hello/src/hello.c
@@ -1,44 +1,49 @@
+// カーソル移動できる Hello world! 表示. ESC か Q で終了.
 #if defined(USE_PDCURSES)
 #include <curses.h>
-#define CLEAR()		clear()
+#define CLEAR()     clear()                             // pdcurses での描画バッファ消去.
 #else
 #include <ncurses.h>
-#define CLEAR()		erase()
+#define CLEAR()     erase()                             // ncurses での描画バッファ消去.
 #endif
 
 int main(void) {
-	int w, h, x, y, x0, y0, t, col = 0, count = 0;
-    initscr();
-    noecho();
-    cbreak();
-    keypad(stdscr, TRUE);
-    curs_set(0);
-    timeout(50);
-	if (has_colors()) {
-	    start_color();
-	    init_pair( 1,COLOR_CYAN,  COLOR_BLACK);
-	    init_pair( 2,COLOR_WHITE, COLOR_BLACK);
-	    col = 1;
+    enum { N = 12};                                     // Hello world! 文字数.
+    int  c, x, y, w, h, col = 0, count = 0;
+    initscr();                                          // curses (スクリーン)初期化.
+    noecho();                                           // キー入力で表示を行わない.
+    cbreak();                                           // 入力バッファリングしない.
+    keypad(stdscr, TRUE);                               // カーソルキーを有効にする.
+    curs_set(0);                                        // カーソルを表示しない.
+    timeout(50);                                        // 50ミリ秒でgetchをタイムアウトさせる指定.
+    if (has_colors()) {                                 // 色有りのスクリーンなら色設定.
+        start_color();
+        init_pair( 1, COLOR_WHITE, COLOR_BLACK);        // 色ペア1を白に.
+        init_pair( 2, COLOR_CYAN,  COLOR_BLACK);        // 色ペア2を水色に.
+        col = 1;
     }
-    getmaxyx(stdscr, h, w);
-	x0 = (w - 12) / 2;
-	y0 = (h -  1) / 2;
-	do {
-		CLEAR();
-		t = count >> 1;
-		x = -8 + ((t & 0x10) ? 15 - (t & 15) : (t & 15));
-		y = -4 + ((t &  0x8) ?  7 - (t &  7) : (t &  7));
-		move(y0+y, x0+x);
-		if (col) {
-			col = (count & 0x0c) ? 1 : 2;
-			attron(COLOR_PAIR(col));
-		}
-		addstr("hello world!");
-		if (col)
-			attroff(COLOR_PAIR(col));
-		++count;
-	} while (getch() == ERR);
+    getmaxyx(stdscr, h, w);                             // 画面サイズ取得.
+    x = (w - N) / 2;                                    // x初期位置:画面中央.
+    y = (h - 1) / 2;                                    // y初期位置:画面中央.
+    for (;;) {
+        c = getch();                                    // 1文字入力. 50ミリ秒でタイムアウト.
+        if (c == 0x1b || c == 'q' || c == 'Q')          // ESCまたは Q キーで終了.
+            break;
+        x = x - (c == KEY_LEFT) + (c == KEY_RIGHT);     // 左右カーソルキーで増減.
+        y = y - (c == KEY_UP  ) + (c == KEY_DOWN);      // 上下カーソルキーで増減.
+        x = (x < 0) ? 0 : (x > w - N) ? (w - N) : x;    // x移動範囲チェック.
+        y = (y < 0) ? 0 : (y > h - 1) ? (h - 1) : y;    // y移動範囲チェック.
 
-	endwin();
-	return 0;
+        CLEAR();                                        // 画面バッファクリア.
+        move(y, x);                                     // 表示位置設定.
+        if (col) {
+            col = (count & 0x0c) ? 1 : 2;               // フレーム数依存で色変更.
+            attron(COLOR_PAIR(col));                    // 色設定.
+        }
+        addstr("Hello world!");                         // hello world!表示.
+        ++count;                                        // フレームカウンタ更新.
+        refresh();                                      // 画面バッファを実画面に反映.
+    }
+    endwin();                                           // curses 終了.
+    return 0;
 }
